554c9bad51f7b6eba708b7a1a9ce9c4b1e482676
tohako <tohako12345@gmail.com>
2025-02-19 06:03:19 +0900

    リファクタ

--- js/script.js
+++ js/script.js
@@ -1,9 +1,55 @@
 // ================================
 // ナビゲーション関連ロジック
 // ================================
+
 //ナビゲーション展開クラス切り替えメソッド
 function toggleNavOpenClass(isNavOpen = null) {
   $('body').toggleClass('is-nav-open', isNavOpen);
+}
+
+//aタグのhrefで指定したIDの要素にスクロールするメソッド
+function scrollToTargetElement(scrollToElementId) {
+  //要素のIDからスクロール位置を返す
+  const calculateScrollPosition = (targetElementId) => {
+    //空もしくは"#"のみ場合はページTOPへ
+    if (targetElementId === '' || targetElementId === '#') {
+      return 0;
+    }
+
+    const targetElement = document.querySelector(targetElementId);
+
+    // ヘッダーにセクションのh2が隠れないようにoffsetで調整している
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const position = targetElement.offsetTop;
+    const offsetManual = 40; //空白微調整したい時用
+    //TODO:できたら自動的に要素が画面中央らへんに収まるコード書いてみる
+
+    return position - (headerHeight + offsetManual);
+  };
+
+  //スクロールアニメーション実行
+  const startScrollAnimation = (posY) => {
+    //NOTE:
+    // ここは、アニメーション操作のみ使い勝手がいいjQuery
+    $('html').animate(
+      {
+        scrollTop: posY,
+      },
+      {
+        duration: 300,
+      }
+    );
+  };
+
+  if (scrollToElementId === '') {
+    console.error('nav a[href]にリンクが未設定');
+    return;
+  }
+
+  toggleNavOpenClass(false);
+
+  const pos = calculateScrollPosition(scrollToElementId);
+  startScrollAnimation(pos);
 }
 
 //ハンバーガーメニュー開閉処理
@@ -11,49 +57,14 @@ $('.nav_btn').on('click', () => {
   toggleNavOpenClass();
 });
 
-//aタグから各セクションへのスクロール処理
 document.querySelectorAll('nav a[href]').forEach((link) => {
   link.addEventListener('click', (e) => {
     e.preventDefault(); //デフォルトaタグの遷移イベントを切る
-
-    const calculateScrollPosition = (element) => {
-      // ヘッダーにセクションのh2が隠れないようにoffsetで調整している
-      const headerHeight = document.querySelector('header').offsetHeight;
-      const position = element.offsetTop;
-      const offsetManual = 40; //空白微調整したい時用
-
-      return position - (headerHeight + offsetManual);
-    };
-    const startScrollAnimation = (posY) => {
-      //NOTE:
-      // アニメーション部分のみ使い勝手がいいjQueryにしてみる
-      $('html').animate(
-        {
-          scrollTop: posY,
-        },
-        {
-          duration: 300,
-        }
-      );
-    };
-
-    const targetId = e.target.getAttribute('href');
-    if (targetId === '') {
-      console.error('nav a[href]にリンクが未設定');
-      return;
-    }
-
-    toggleNavOpenClass(false);
-
-    //"#"のみ場合はページTOP
-    if (targetId === '#') {
-      startScrollAnimation(0);
-      return;
-    }
-
-    const targetElm = document.querySelector(targetId);
-
-    const pos = calculateScrollPosition(targetElm);
-    startScrollAnimation(pos);
+    scrollToTargetElement(e.target.getAttribute('href'));
   });
+});
+
+document.querySelector('header h1 a').addEventListener('click', (e) => {
+  e.preventDefault(); //デフォルトaタグの遷移イベントを切る
+  scrollToTargetElement(e.target.getAttribute('href'));
 });
