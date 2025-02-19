78d190985cc659cfa5d2484c788bc28d4122e1f2
tohako <tohako12345@gmail.com>
2025-02-20 04:38:28 +0900

    SP版ビューポートアニメーション実装

--- js/script.js
+++ js/script.js
@@ -108,15 +108,43 @@ $('#mainvisual').slick({
 // PHOTOのフェードイン
 // ===============================
 const $photoList = $('.photo_list');
+const $photoCards = $('.photo_card');
+
 $(window).on('scroll', () => {
-  const isInView = $photoList.inView('topOnly', 150);
-  if (isInView && !$photoList.hasClass('in-view')) {
+  const viewportWidth = window.innerWidth;
+
+  //in-viewのclassを付与
+  const attachInViewClass = ($jqElement) => {
+    if ($jqElement.hasClass('in-view')) {
+      return;
+    }
+
     //ビューポートアニメーション
-    $photoList.addClass('in-view');
+    $jqElement.addClass('in-view');
 
     //回転のアニメーション遅延実行
     setTimeout(() => {
-      $photoList.addClass('in-view-after');
+      $jqElement.addClass('in-view-after');
     }, 1500);
+  };
+
+  //スマホの場合
+  if (viewportWidth <= breakPointSp) {
+    //各カード要素個別にアニメーションさせる
+    $photoCards.each(function () {
+      const $card = $(this);
+      const isInView = $card.inView('topOnly', 100);
+      if (isInView) {
+        attachInViewClass($card);
+      }
+    });
+  }
+  //PCの場合
+  else {
+    //カードの親要素に掛かったらアニメーション開始
+    const isInView = $photoList.inView('topOnly', 150);
+    if (isInView) {
+      attachInViewClass($photoList);
+    }
   }
 });
